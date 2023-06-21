bc4fc3f086a806d2036ce71183bb9ebcf4bf78b8
isa-ismail <fahimismail75.scc@gmail.com>
2023-06-21 17:43:59 +0600

    13

---
+++ app/ashes/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    children: React.ReactElement
+}
+
+const layout:React.FC<Props> = ({children}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default layout
---
+++ app/ashes/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { GetServerSideProps } from 'next'
+
+interface Props {
+
+}
+
+//fetch data from api
+
+const page:React.FC<Props> = () => {
+    return (
+        <div>
+            ashedComponent
+        </div>
+    )
+}
+
+export default page
